8b6d335f0698aea67f877bbdf23d845210d1eb88
Ocknyer <kdu4045@naver.com>
2023-11-20 11:48:45 +0900

    naver map error ref

--- components/NaverMap.tsx
+++ components/NaverMap.tsx
@@ -1,12 +1,15 @@
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 
 const NaverMap = () => {
+  const mapElement = useRef(null);
+  const { naver } = window;
+
   useEffect(() => {
-    if (!naver) return;
+    if (!mapElement.current || !naver) return;
 
     const location = new naver.maps.LatLng(37.5580297, 126.935877);
 
-    const map = new naver.maps.Map('map', {
+    const map = new naver.maps.Map(mapElement.current, {
       center: location,
       zoomControl: true,
       zoom: 15,
@@ -22,7 +25,7 @@ const NaverMap = () => {
     });
   }, []);
 
-  return <div id='map' style={{ width: '300px', height: '200px' }}></div>;
+  return <div ref={mapElement} id='map' style={{ width: '300px', height: '200px' }}></div>;
 };
 
 export default NaverMap;
